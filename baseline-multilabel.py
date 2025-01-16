1e7dd9e957b7aec1385d238a627b82ed3836eff8
Littleote <david.candelar@gmail.com>
2025-01-16 15:31:42 +0100

    Update multilabel baseline

--- baseline-multilabel.py
+++ baseline-multilabel.py
@@ -1,32 +1,13 @@
 import numpy as np
 import pacmap
 import pandas as pd
-from sklearn.model_selection import KFold
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import hamming_loss
+from sklearn.metrics import label_ranking_average_precision_score as lrap
+from sklearn.model_selection import KFold
 from statsmodels import api as sm
 
 from dataset import load_npy
-
-
-def f1_score(stats):
-    F1 = {
-        "positive_F1": 2
-        * stats["true_positive"]
-        / (
-            2 * stats["true_positive"]
-            + stats["false_positive"]
-            + stats["false_negative"]
-        ),
-        "negaitve_F1": 2
-        * stats["true_negative"]
-        / (
-            2 * stats["true_negative"]
-            + stats["false_positive"]
-            + stats["false_negative"]
-        ),
-    }
-    F1["mean_F1"] = (F1["positive_F1"] + F1["negaitve_F1"]) / 2
-    return F1
 
 
 def glm(X, y, *, verbose: bool = False):
@@ -35,20 +16,24 @@ def glm(X, y, *, verbose: bool = False):
     z = epsilon + (1 - 2 * epsilon) * y
 
     kfold = KFold(n_splits=10, shuffle=True, random_state=42)
-    y_hat = np.zeros(y.shape)
+    p_hat = np.zeros(y.shape, dtype=float)
+    y_hat = np.zeros(y.shape, dtype=bool)
     for train, test in kfold.split(X, y):
         model = sm.GLM(z[train], X[train], family=sm.families.Binomial()).fit()
-        y_hat[test] = np.round(model.predict(X[test])).astype(bool)
-    return y_hat
+        p_hat[test] = model.predict(X[test])
+        y_hat[test] = np.round(p_hat[test]).astype(bool)
+    return y_hat, p_hat
 
 
 def rf(X, y, *, verbose: bool = False, **info):
     kfold = KFold(n_splits=10, shuffle=True, random_state=42)
-    y_hat = np.zeros(y.shape)
+    p_hat = np.zeros(y.shape, dtype=float)
+    y_hat = np.zeros(y.shape, dtype=bool)
     for train, test in kfold.split(X, y):
         model = RandomForestClassifier(random_state=42).fit(X[train], y[train])
-        y_hat[test] = np.round(model.predict(X[test])).astype(bool)
-    return y_hat
+        p_hat[test] = model.predict_proba(X[test])[:, 1]
+        y_hat[test] = model.predict(X[test]).astype(bool)
+    return y_hat, p_hat
 
 
 def alpha_score(Yx, Px, alpha, beta, gamma):
@@ -59,7 +44,8 @@ def alpha_score(Yx, Px, alpha, beta, gamma):
     return score
 
 
-def measures(Yx, Px, score, names):
+def measures(Yx, Px, prob, names, **alphas):
+    score = alpha_score(Yx, Px, **alphas)
     accuracy = np.mean(score)
     precision = np.sum(score[:, np.newaxis] * Px, axis=0) / np.sum(Px, axis=0)
     recall = np.sum(score[:, np.newaxis] * Yx, axis=0) / np.sum(Yx, axis=0)
@@ -67,6 +53,8 @@ def measures(Yx, Px, score, names):
         {"accuracy -": accuracy}
         | {f"precision {name}": p for p, name in zip(precision, names)}
         | {f"recall {name}": r for r, name in zip(recall, names)}
+        | {"Hamming-Loss -": hamming_loss(Yx, Px)}
+        | {"LRAP -": lrap(Yx, prob)}
     )
     return results
 
@@ -85,18 +73,23 @@ if __name__ == "__main__":
     y_root4 = np.zeros(y.shape, dtype=bool)
     y_lowdim = np.zeros(y.shape, dtype=bool)
     y_rf = np.zeros(y.shape, dtype=bool)
+    p_glm = np.zeros(y.shape, dtype=float)
+    p_root4 = np.zeros(y.shape, dtype=float)
+    p_lowdim = np.zeros(y.shape, dtype=float)
+    p_rf = np.zeros(y.shape, dtype=float)
     results = []
     for idx in range(len(labels)):
         y_label = y[:, idx]
-        y_glm[:, idx] = glm(X, y_label)
-        y_root4[:, idx] = glm(X_root4, y_label)
-        y_lowdim[:, idx] = glm(X_lowdim, y_label)
-        y_rf[:, idx] = rf(X, y_label)
+        y_glm[:, idx], p_glm[:, idx] = glm(X, y_label)
+        y_root4[:, idx], p_root4[:, idx] = glm(X_root4, y_label)
+        y_lowdim[:, idx], p_lowdim[:, idx] = glm(X_lowdim, y_label)
+        y_rf[:, idx], p_rf[:, idx] = rf(X, y_label)
+    params = dict(alpha=1, beta=1, gamma=0.25)
     results = [
-        measures(y, y_glm, alpha_score(y, y_root4, 1, 1, 0.25), labels),
-        measures(y, y_root4, alpha_score(y, y_root4, 1, 1, 0.25), labels),
-        measures(y, y_lowdim, alpha_score(y, y_root4, 1, 1, 0.25), labels),
-        measures(y, y_rf, alpha_score(y, y_root4, 1, 1, 0.25), labels),
+        measures(y, y_glm, p_glm, labels, **params),
+        measures(y, y_root4, p_root4, labels, **params),
+        measures(y, y_lowdim, p_lowdim, labels, **params),
+        measures(y, y_rf, p_rf, labels, **params),
     ]
     df = pd.DataFrame(results)
     df.to_csv("baseline-multilabel.csv", index=False)
